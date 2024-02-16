b8fbaf4acdae1da1622a044b3c8d4889e58dcfad
arpan <arpan.rec@gmail.com>
2024-02-16 18:15:33 +0530

    nvim lua

--- .config/nvim/lua/base/packer.lua
+++ .config/nvim/lua/base/packer.lua
@@ -30,7 +30,7 @@ return require('packer').startup(function(use)
 	}
 	use {
 		'nvim-treesitter/nvim-treesitter',
---		run = ':TSUpdate'
+		-- run = ':TSUpdate'
 	}
 
 -- Automatically set up your configuration after cloning packer.nvim

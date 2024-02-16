81c06fd1466ffca1123e7838dcf1fee0143defe2
arpan <arpan.rec@gmail.com>
2024-02-16 18:25:09 +0530

    nvim lua

--- .config/nvim/lua/base/packer.lua
+++ .config/nvim/lua/base/packer.lua
@@ -32,7 +32,7 @@ return require('packer').startup(function(use)
 		'nvim-treesitter/nvim-treesitter',
 		-- run = ':TSUpdate'
 	}
-
+	use { 'mbbill/undotree' }
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
 if packer_bootstrap then
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -104,6 +104,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/arpan/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  undotree = {
+    loaded = true,
+    path = "/home/arpan/.local/share/nvim/site/pack/packer/start/undotree",
+    url = "https://github.com/mbbill/undotree"
   }
 }
 

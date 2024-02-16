cdda09708e69d61f0adc7e91b2e32a7b4077354d
arpan <arpan.rec@gmail.com>
2024-02-16 18:08:36 +0530

    nvim lua

---
+++ .config/nvim/after/plugin/colors.lua
@@ -0,0 +1,9 @@
+function ColorMyPencils()
+
+	colors = color or "rose-pine"
+	vim.cmd.colorscheme(colors)
+	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+end
+
+ColorMyPencils()

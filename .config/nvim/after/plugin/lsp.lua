2aadfae77d1591a264ed70d9e3aeb3cd92c11691
arpan <arpan.rec@gmail.com>
2024-02-16 19:17:03 +0530

    nvim lua

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -17,6 +17,7 @@ end)
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+require'lspconfig'.jedi_language_server.setup{}
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {'tsserver', 'rust_analyzer'},

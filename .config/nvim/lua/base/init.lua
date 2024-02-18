7fff9c64c49839414590af3f2787367e3fa392c5
arpan <arpan.rec@gmail.com>
2024-02-19 00:43:15 +0530

    Update key mappings and settings in init.lua

--- .config/nvim/lua/base/init.lua
+++ .config/nvim/lua/base/init.lua
@@ -15,7 +15,6 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.softtabstop = 4
 
-
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
@@ -26,7 +25,6 @@ vim.opt.undodir = vim.fn.stdpath("data") .. "/.vim/undodir"
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
@@ -36,6 +34,23 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "120"
 
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+

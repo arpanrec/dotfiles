79e763360f2efb6b0486fbae90a479bb5521960f
arpan <arpan.rec@gmail.com>
2024-02-19 00:28:11 +0530

    Update Neovim configuration

--- .config/nvim/lua/base/init.lua
+++ .config/nvim/lua/base/init.lua
@@ -1,2 +1,41 @@
 require("base.packer")
-require("base.remap")
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.opt.guicursor = ""
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.cursorline = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "/.vim/undodir"
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "120"
+
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")

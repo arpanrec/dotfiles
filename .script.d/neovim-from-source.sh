67efaaa73bfb5481b3451b2e9ddcff3be85a308d
arpan <arpan.rec@gmail.com>
2024-02-21 02:14:46 +0530

    Add script to install Neovim from source

---
+++ .script.d/neovim-from-source.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -ex
+
+NEOVIM_GIT_CLONE_DIR="${NEOVIM_GIT_CLONE_DIR:-"/tmp/neovim"}"
+NEOVIM_INSTALL_DIR="${NEOVIM_INSTALL_DIR:-"${HOME}/.local"}"
+
+neovim_install() {
+    pwd
+    make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=${NEOVIM_INSTALL_DIR}"
+    make install
+}
+
+rm -rf "${NEOVIM_GIT_CLONE_DIR}"
+git clone https://github.com/neovim/neovim.git --single-branch --branch=stable --depth 1 "${NEOVIM_GIT_CLONE_DIR}"
+
+cd "${NEOVIM_GIT_CLONE_DIR}"
+neovim_install

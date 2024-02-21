acec20cb6491a618d3f3b311a7f6d94259fc9807
arpan <arpan.rec@gmail.com>
2024-02-21 23:51:10 +0530

    Remove unnecessary Neovim directories and files

--- .script.d/neovim-from-source.sh
+++ .script.d/neovim-from-source.sh
@@ -5,7 +5,14 @@ NEOVIM_GIT_CLONE_DIR="${NEOVIM_GIT_CLONE_DIR:-"/tmp/neovim-src-$(date +%s)"}"
 NEOVIM_INSTALL_DIR="${NEOVIM_INSTALL_DIR:-"${HOME}/.local"}"
 
 neovim_install() {
-    pwd
+
+    rm -rf "${HOME}/.local/bin/nvim"
+    rm -rf "${HOME}/.local/state/nvim"
+    rm -rf "${HOME}/.local/share/nvim"
+    rm -rf "${HOME}/.cache/nvim"
+    rm -rf "${HOME}/.local/lib/nvim"
+    rm -rf "${HOME}/.local/neovim"
+
     make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=${NEOVIM_INSTALL_DIR}"
     make install
 }

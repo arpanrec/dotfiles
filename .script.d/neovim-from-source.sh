b5a6db4bee41785bf11a9ca861e6f8bdb6c99791
arpan <arpan.rec@gmail.com>
2024-02-25 01:32:12 +0530

    Update Neovim installation script

--- .script.d/neovim-from-source.sh
+++ .script.d/neovim-from-source.sh
@@ -6,10 +6,9 @@ NEOVIM_INSTALL_DIR="${NEOVIM_INSTALL_DIR:-"${HOME}/.local"}"
 
 neovim_install() {
 
-    rm -rf "${HOME}/.local/*/nvim"
-    rm -rf "${HOME}/.local/*/nvim*"
+    rm -rf "${HOME}/.local/state/nvim"
+    rm -rf "${HOME}/.local/share/nvim"
     rm -rf "${HOME}/.cache/nvim"
-    rm -rf "${HOME}/.cache/nvim*"
 
     make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=${NEOVIM_INSTALL_DIR}"
     make install
@@ -17,6 +16,6 @@ neovim_install() {
 
 mkdir -p "$(dirname "${NEOVIM_GIT_CLONE_DIR}")"
 git clone https://github.com/neovim/neovim.git --single-branch --branch=stable --depth 1 "${NEOVIM_GIT_CLONE_DIR}"
-
 cd "${NEOVIM_GIT_CLONE_DIR}"
+
 neovim_install

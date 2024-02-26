d18e7cb689d04020796386e7f7bc7a54b75443b5
arpan <arpan.rec@gmail.com>
2024-02-27 00:42:35 +0530

    Remove unnecessary plugins and update lazy.nvim setup

--- .config/nvim/lua/arpanrec/lazy.lua
+++ .config/nvim/lua/arpanrec/lazy.lua
@@ -1,18 +1,44 @@
+-- if ~/.ssh/id_rsa_gitlab exists, set git clone url to ssh instead of https
+local lazy_git_url = "https://github.com/%s.git"
+
+if vim.fn.filereadable(vim.fn.expand("~/.ssh/id_rsa_gitlab")) == 1 then
+    lazy_git_url = "git@github.com:%s.git"
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
         "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
+        lazy_git_url:format("folke/lazy.nvim"),
+        "--branch=stable",
         lazypath,
     })
 end
+
 vim.opt.rtp:prepend(lazypath)
--- local Util = require("lazyvim.util")
+
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-require("lazy").setup("arpanrec.plugins")
+
+require("lazy").setup({ { import = "arpanrec.plugins" }, { import = "arpanrec.plugins.lsp" } }, {
+    install = {
+        colorscheme = { "nightfly" },
+    },
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        notify = false,
+    },
+    git = {
+        log = { "-8" },
+        timeout = 120,
+        url_format = lazy_git_url,
+        filter = true,
+    },
+})
 
 -- -- auto pairs
 -- {

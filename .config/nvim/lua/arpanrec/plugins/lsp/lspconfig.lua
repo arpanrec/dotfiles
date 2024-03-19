e1a806f1b787435529280735e6429799469bf336
arpan <arpan.rec@gmail.com>
2024-03-19 10:51:37 +0530

    LSP Keymap

--- .config/nvim/lua/arpanrec/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/arpanrec/plugins/lsp/lspconfig.lua
@@ -18,54 +18,48 @@ return {
         local on_attach = function(client, bufnr)
             opts.buffer = bufnr
 
-            -- local keymap = vim.keymap -- for conciseness
-
             -- set keybinds
             -- opts.desc = "Show LSP references"
-            -- keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+            -- vim.keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
             --
             -- opts.desc = "Go to declaration"
-            -- keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+            -- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
             --
             -- opts.desc = "Show LSP definitions"
-            -- keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+            -- vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
             --
             -- opts.desc = "Show LSP implementations"
-            -- keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+            -- vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
             --
             -- opts.desc = "Show LSP type definitions"
-            -- keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+            -- vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
             --
             -- opts.desc = "See available code actions"
-            -- keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+            -- vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
             --
             -- opts.desc = "Smart rename"
-            -- keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+            -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
             --
             -- opts.desc = "Show buffer diagnostics"
-            -- keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-            --
-            -- opts.desc = "Show line diagnostics"
-            -- keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-            --
-            -- opts.desc = "Go to previous diagnostic"
-            -- keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-            --
-            -- opts.desc = "Go to next diagnostic"
-            -- keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-            --
-            -- opts.desc = "Show documentation for what is under cursor"
-            -- keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-            --
-            -- opts.desc = "Restart LSP"
-            -- keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+            -- vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+            opts.desc = "Show line diagnostics"
+            vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+            opts.desc = "Go to previous diagnostic"
+            vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+            opts.desc = "Go to next diagnostic"
+            vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+            opts.desc = "Show documentation for what is under cursor"
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+            opts.desc = "Restart LSP"
+            vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
             vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
             vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
             vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
             vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
@@ -99,21 +93,20 @@ return {
             capabilities = capabilities,
             on_attach = on_attach,
             settings = { -- custom settings for lua
-            Lua = {
-                -- make the language server recognize "vim" global
-                diagnostics = {
-                    globals = { "vim" },
-                },
-                workspace = {
-                    -- make language server aware of runtime files
-                    library = {
-                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                        [vim.fn.stdpath("config") .. "/lua"] = true,
+                Lua = {
+                    -- make the language server recognize "vim" global
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                    workspace = {
+                        -- make language server aware of runtime files
+                        library = {
+                            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                            [vim.fn.stdpath("config") .. "/lua"] = true,
+                        },
                     },
                 },
             },
-        },
-    })
-
-end,
+        })
+    end,
 }

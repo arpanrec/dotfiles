eafc8e5234d985c7adc641525b12b36823c93d01
arpan <arpan.rec@gmail.com>
2024-03-21 13:02:49 +0530

    treesitter context

--- .config/nvim/lua/arpanrec/plugins/treesitter.lua
+++ .config/nvim/lua/arpanrec/plugins/treesitter.lua
@@ -1,13 +1,18 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        { "nvim-treesitter/nvim-treesitter-context" },
+        { "nvim-treesitter/nvim-treesitter-refactor" },
+        { "nvim-treesitter/nvim-treesitter-textobjects" },
+    },
     run = ":TSUpdate",
     lazy = false,
     config = function()
-        require 'nvim-treesitter.configs'.setup({
+        require("nvim-treesitter.configs").setup({
             ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "go", "bash", "tsx",
                 "json", "javascript", "typescript", "html", "css", "yaml", "rust", "dockerfile", "graphql", "jsonc", },
-
             sync_install = false,
+            modules = {},
             ignore_install = {},
             auto_install = true,
             highlight = {
@@ -16,6 +21,55 @@ return {
             },
             indent = {
                 enable =true,
+            },
+            refactor = {
+                highlight_definitions = {
+                    enable = true,
+                    -- Set to false if you have an `updatetime` of ~100.
+                    clear_on_cursor_move = true,
+                },
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+
+                    -- Automatically jump forward to textobj, similar to targets.vim
+                    lookahead = true,
+
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                        -- nvim_buf_set_keymap) which plugins like which-key display
+                        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                        -- You can also use captures from other query groups like `locals.scm`
+                        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                    },
+                    -- You can choose the select mode (default is charwise 'v')
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * method: eg 'v' or 'o'
+                    -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                    -- mapping query_strings to modes.
+                    selection_modes = {
+                        ['@parameter.outer'] = 'v', -- charwise
+                        ['@function.outer'] = 'V', -- linewise
+                        ['@class.outer'] = '<c-v>', -- blockwise
+                    },
+                    -- If you set this to `true` (default is `false`) then any textobject is
+                    -- extended to include preceding or succeeding whitespace. Succeeding
+                    -- whitespace has priority in order to act similarly to eg the built-in
+                    -- `ap`.
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * selection_mode: eg 'v'
+                    -- and should return true or false
+                    include_surrounding_whitespace = true,
+                },
             },
         })
     end,

2701686d1087b6cdfd770e5c9df3f78166ff77a9
arpan <arpan.rec@gmail.com>
2024-03-24 21:46:08 +0530

    json and yaml schema

--- .config/nvim/lua/arpanrec/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/arpanrec/plugins/lsp/lspconfig.lua
@@ -144,7 +144,39 @@ return {
         lspconfig.cssmodules_ls.setup({ capabilities = capabilities, on_attach = on_attach, })
 
 
-        lspconfig.jsonls.setup({ capabilities = capabilities, on_attach = on_attach, })
+        lspconfig.jsonls.setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            filetypes = { "json", "jsonc" },
+            settings = {
+                json = {
+                    schemas = {
+                        { fileMatch = { "package.json" },   url = "https://json.schemastore.org/package.json" },
+                        { fileMatch = { "tsconfig*.json" }, url = "https://json.schemastore.org/tsconfig.json" },
+                        {
+                            fileMatch = { ".prettierrc", ".prettierrc.json", "prettier.config.json" },
+                            url = "https://json.schemastore.org/prettierrc.json"
+                        },
+                        { fileMatch = { ".eslintrc", ".eslintrc.json" }, url = "https://json.schemastore.org/eslintrc.json" },
+                        {
+                            fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+                            url = "https://json.schemastore.rg/babelrc.json"
+                        },
+                        { fileMatch = { "lerna.json" },                  url = "https://json.schemastore.org/lerna.json" },
+                        { fileMatch = { "now.json", "vercel.json" },     url = "https://json.schemastore.org/now.json" },
+                        {
+                            fileMatch = {
+                                ".stylelintrc",
+                                ".stylelintrc.json",
+                                "stylelint.config.json"
+                            },
+                            url = "http://json.schemastore.org/stylelintrc.json"
+                        }
+                    }
+                }
+            }
+
+        })
 
         lspconfig.gopls.setup({ capabilities = capabilities, on_attach = on_attach, })
 
@@ -183,6 +215,28 @@ return {
 
         lspconfig.ansiblels.setup({ capabilities = capabilities, on_attach = on_attach, })
 
-        lspconfig.yamlls.setup({ capabilities = capabilities, on_attach = on_attach, })
+        lspconfig.yamlls.setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = {
+                yaml = {
+                    schemas = {
+                        ["http://json.schemastore.org/gitlab-ci.json"] = { ".gitlab-ci.yml" },
+                        ["https://json.schemastore.org/bamboo-spec.json"] = {
+                            "bamboo-specs/*.{yml,yaml}"
+                        },
+                        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = {
+                            "docker-compose*.{yml,yaml}"
+                        },
+                        ["http://json.schemastore.org/github-workflow.json"] = ".github/workflows/*.{yml,yaml}",
+                        ["http://json.schemastore.org/github-action.json"] = ".github/action.{yml,yaml}",
+                        ["http://json.schemastore.org/prettierrc.json"] = ".prettierrc.{yml,yaml}",
+                        ["http://json.schemastore.org/stylelintrc.json"] = ".stylelintrc.{yml,yaml}",
+                        ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}"
+                    }
+                }
+            }
+
+        })
     end,
 }

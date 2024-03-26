d3b0a3ae0acf9ec79942eea35c4b77715d348c75
Arpan Mandal <arpan.rec@gmail.com>
2024-03-26 11:26:29 +0530

    eslint

--- .config/nvim/lua/arpanrec/plugins/lsp/mason.lua
+++ .config/nvim/lua/arpanrec/plugins/lsp/mason.lua
@@ -27,7 +27,7 @@ return {
         mason_lspconfig.setup({
             -- list of servers for mason to install
             ensure_installed = { "lua_ls", "gopls", "pyright", "marksman", "bashls",
-                "pyright", "tsserver", "jsonls",
+                "pyright", "tsserver", "jsonls", "eslint"
                 "html", "cssls", "tailwindcss", "svelte", "graphql",
                 "emmet_ls", "prismals", "ansiblels", "yamlls",
                 "cssmodules_ls", },

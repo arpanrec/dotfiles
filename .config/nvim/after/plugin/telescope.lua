79b0b8744f3aadfb4b997ef4bd24258d781a99bd
Arpan Mandal <arpan.rec@gmail.com>
2024-03-29 17:12:35 +0530

    feat: `<leader>fa` telescope keymap find all and some minor changes
    `<leader>fa` telescope keymap find all
    
    Telescope look for hidden files
    
    Telescope ignore node_modules, venv, .venv, .git

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,10 +1,15 @@
-local builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files in project directory" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep, searches a string in entire project" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find open buffers" })
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find vim help apis and docs" })
-vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Find files which are present in git" })
+local telescope = require("telescope.builtin")
+vim.keymap.set("n", "<leader>fa",
+    function()
+        telescope.find_files({ hidden = true, no_ignore = true, no_ignore_parent = true })
+    end,
+    { desc = "Find all files" })
+vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Smart find files in project directory" })
+vim.keymap.set("n", "<leader>fg", telescope.live_grep,
+    { desc = "Live grep, searches a string in entire project" })
+vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "Find open buffers" })
+vim.keymap.set("n", "<leader>fh", telescope.help_tags, { desc = "Find vim help apis and docs" })
+vim.keymap.set("n", "<C-p>", telescope.git_files, { desc = "Find files which are present in git" })
 
 vim.keymap.set("n", "<leader>ps", function()
     if vim.fn.executable("rg") == 0 then
@@ -12,7 +17,7 @@ vim.keymap.set("n", "<leader>ps", function()
         return
     end
 
-    builtin.grep_string({
+    telescope.grep_string({
         search = vim.fn.input("Grep String > "),
     })
 end, {
--- .config/nvim/lua/arpanrec/plugins/telescope.lua
+++ .config/nvim/lua/arpanrec/plugins/telescope.lua
@@ -6,33 +6,11 @@ return {
         { "nvim-telescope/telescope-ui-select.nvim" },
     },
     lazy = false,
-
     config = function()
         require("telescope").setup({
-            extensions = {
-                ["ui-select"] = {
-                    require("telescope.themes").get_dropdown({
-                        -- even more opts
-                    }),
-
-                    -- pseudo code / specification for writing custom displays, like the one
-                    -- for "codeactions"
-                    -- specific_opts = {
-                    --   [kind] = {
-                    --     make_indexed = function(items) -> indexed_items, width,
-                    --     make_displayer = function(widths) -> displayer
-                    --     make_display = function(displayer) -> function(e)
-                    --     make_ordinal = function(e) -> string
-                    --   },
-                    --   -- for example to disable the custom builtin "codeactions" display
-                    --      do the following
-                    --   codeactions = false,
-                    -- }
-                },
-            },
+            defaults = { file_ignore_patterns = { "node_modules", "venv", ".venv,", ".git" } },
+            pickers = { find_files = { hidden = false } }
         })
-        -- To get ui-select loaded and working with telescope, you need to call
-        -- load_extension, somewhere after setup function:
-        require("telescope").load_extension("ui-select")
+        -- telescope.load_extension("ui-select")
     end,
 }

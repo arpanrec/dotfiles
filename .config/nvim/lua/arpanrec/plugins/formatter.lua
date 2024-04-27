adad0cf6022ef3372262b70ab39aa43aece1910c
arpan <arpan.rec@gmail.com>
2024-04-27 17:59:49 +0530

    disable autoformat on save

--- .config/nvim/lua/arpanrec/plugins/formatter.lua
+++ .config/nvim/lua/arpanrec/plugins/formatter.lua
@@ -5,11 +5,11 @@ return {
     config = function()
         require("conform").setup({
             log_level = vim.log.levels.DEBUG,
-            format_on_save = {
-                timeout_ms = 3000,
-                async = false,
-                quiet = false,
-            },
+            -- format_on_save = {
+            --     timeout_ms = 3000,
+            --     async = false,
+            --     quiet = false,
+            -- },
             formatters_by_ft = {
                 javascript = { "prettier" },
                 typescript = { "prettier" },

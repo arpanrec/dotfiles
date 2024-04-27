8876e40d0684310ed7ba0a74db717cacc5b9d34b
arpan <arpan.rec@gmail.com>
2024-04-27 15:55:54 +0530

    Formatter

--- .config/nvim/after/plugin/trouble.lua
+++ .config/nvim/after/plugin/trouble.lua
@@ -1,8 +1,10 @@
 -- luacheck: globals vim
 
-vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end, { desc = "Toggle trouble" })
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end,
+    { desc = "Toggle workspace diagnostics" })
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end,
+    { desc = "Toggle document diagnostics" })
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end, { desc = "Toggle quickfix" })
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end, { desc = "Toggle loclist" })
+vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end, { desc = "Toggle lsp references" })
--- .config/nvim/lua/arpanrec/plugins/formatter.lua
+++ .config/nvim/lua/arpanrec/plugins/formatter.lua
@@ -1,23 +1,32 @@
 -- luacheck: globals vim
 
 return {
-    "mhartington/formatter.nvim",
-    version = nil,
+    "stevearc/conform.nvim",
     config = function()
-        -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-        require("formatter").setup({
-            logging = true,
-            log_level = vim.log.levels.WARN,
-            filetype = {
-                lua = { require("formatter.filetypes.lua").stylua },
-                yaml = { require("formatter.filetypes.yaml").yamlfmt },
-                python = { require("formatter.filetypes.python").black },
-                json = { require("formatter.filetypes.json").prettier },
-                ["yaml.ansible"] = { require("formatter.filetypes.yaml").yamlfmt },
-            },
-            vim.keymap.set({ "n", "v" }, "<leader>vff", function()
-                vim.cmd(":Format")
-            end, { desc = "Format current file using formatter" }),
+        require("conform").setup({
+            require("conform").setup({
+                log_level = vim.log.levels.DEBUG,
+                format_on_save = {
+                    timeout_ms = 3000,
+                    async = false,
+                    quiet = false,
+                },
+                formatters_by_ft = {
+                    javascript = { "prettier" },
+                    typescript = { "prettier" },
+                    javascriptreact = { "prettier" },
+                    typescriptreact = { "prettier" },
+                    css = { "prettier" },
+                    html = { "prettier" },
+                    json = { "prettier" },
+                    markdown = { "prettier" },
+                    lua = { "stylua" },
+                    python = { "black" },
+                    ["yaml.ansible"] = { "prettier" },
+                    yaml = { "prettier" },
+                },
+            })
+
         })
     end,
 }

53410d90053be16035cf381e2eae10658857993d
arpan <arpan.rec@gmail.com>
2024-04-30 01:39:04 +0530

    inprogress

--- .script.d/debian-cloudinit-ansible.sh
+++ .script.d/debian-cloudinit-ansible.sh
@@ -39,9 +39,10 @@ apt install -y git python3-venv python3-pip
 
 deactivate || true
 
-export ANSIBLE_ROLES_PATH="/tmp/cloudinit/roles"
+export DEFAULT_ROLES_PATH="/tmp/cloudinit/roles"
+export ANSIBLE_ROLES_PATH="${DEFAULT_ROLES_PATH}"
 export ANSIBLE_COLLECTIONS_PATH="/tmp/cloudinit/collections"
-mkdir -p "/tmp/cloudinit" "${ANSIBLE_ROLES_PATH}" "${ANSIBLE_COLLECTIONS_PATH}"
+mkdir -p "/tmp/cloudinit" "${DEFAULT_ROLES_PATH}" "${ANSIBLE_COLLECTIONS_PATH}"
 rm -rf "/tmp/cloudinit/venv"
 python3 -m venv "/tmp/cloudinit/venv"
 source "/tmp/cloudinit/venv/bin/activate"
@@ -49,13 +50,42 @@ pip install ansible --upgrade
 ansible-galaxy collection install git+https://github.com/arpanrec/arpanrec.nebula.git -f
 ansible-galaxy role install git+https://github.com/geerlingguy/ansible-role-docker.git,,geerlingguy.docker -f
 
-tee "/tmp/cloudinit/hosts" <<EOF >/dev/null
-[cloudinit]
-localhost ansible_connection=local
+tee "/tmp/cloudinit/hosts.yml" <<EOF >/dev/null
+all:
+    children:
+        server_workspace:
+            hosts:
+                localhost:
+            vars:
+                ansible_user: "${CLOUD_INIT_USERNAME}"
+        cloudinit:
+            hosts:
+                localhost:
+            vars:
+                ansible_user: root
+                pv_cloud_username: "${CLOUD_INIT_USERNAME}"
+                pv_cloud_is_dev_machine: "${CLOUD_INIT_IS_DEV_MACHINE}"
+                pv_cloud_groupname: "${CLOUD_INIT_GROUPNAME}"
+                pv_cloud_hostname: "${CLOUD_INIT_HOSTNAME}"
+                pv_cloud_domainname: ${CLOUD_INIT_DOMAINNAME}
+                pv_cloud_user_ssh_public_key: ${CLOUD_INIT_USE_SSHPUBKEY}
+    hosts:
+        localhost:
+            ansible_connection: local
+            ansible_python_interpreter: /usr/bin/python3
 EOF
 
-ansible-playbook -i "/tmp/cloudinit/hosts" \
-    --extra-vars "pv_cloud_username=${CLOUD_INIT_USERNAME} pv_cloud_is_dev_machine=${CLOUD_INIT_IS_DEV_MACHINE} \
-    pv_cloud_groupname=${CLOUD_INIT_GROUPNAME} pv_cloud_hostname=${CLOUD_INIT_HOSTNAME} \
-    pv_cloud_domainname=${CLOUD_INIT_DOMAINNAME} pv_cloud_user_ssh_public_key=${CLOUD_INIT_USE_SSHPUBKEY}" \
-    arpanrec.nebula.cloudinit
+# ansible-playbook -i "/tmp/cloudinit/hosts.yml" arpanrec.nebula.cloudinit
+
+chown -R "${CLOUD_INIT_USERNAME}:${CLOUD_INIT_GROUPNAME}" "/tmp/cloudinit"
+
+sudo -H -u "${CLOUD_INIT_USERNAME}" bash -c '
+    set -ex
+    source "/tmp/cloudinit/venv/bin/activate"
+    if [ "${CLOUD_INIT_IS_DEV_MACHINE}" = true ]; then
+        ansible-playbook -i "/tmp/cloudinit/hosts.yml" arpanrec.nebula.server_workspace --tags all
+    else
+        ansible-playbook -i "/tmp/cloudinit/hosts.yml" arpanrec.nebula.server_workspace \
+            --tags all --skip-tags java,bw,go,terraform,vault,nodejs
+    fi
+'

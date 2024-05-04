a05825fa25e0adfb11cc3a4921b4f0e6b307a2b3
arpan <arpan.rec@gmail.com>
2024-05-04 23:12:19 +0530

    install tools

---
+++ .script.d/tools.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -ex
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q -y
+
+# shellcheck source=/dev/null
+source "$HOME/.cargo/env"
+cargo install ripgrep
+
+curl -sSL https://install.python-poetry.org | python3 -
+
+npm i -g yarn
+npm i -g bw
+
+go install golang.org/x/tools/gopls@latest
+go install mvdan.cc/sh/v3/cmd/gosh@latest
+
+"${HOME}/.script.d/neovim-from-source.sh"

8b62fba409608f34d760865132872d2a515c9736
arpan <arpan.rec@gmail.com>
2024-05-04 23:18:31 +0530

    feat: Install additional Cargo packages for improved development workflow

--- .script.d/tools.sh
+++ .script.d/tools.sh
@@ -5,6 +5,9 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q -y
 
 # shellcheck source=/dev/null
 source "$HOME/.cargo/env"
+cargo install cargo-binstall
+cargo install cargo-quickinstall
+cargo install cargo-update
 cargo install ripgrep
 
 curl -sSL https://install.python-poetry.org | python3 -

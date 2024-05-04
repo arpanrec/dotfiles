1d29d3274b8bc673a934a7e61d5031cf74cb0b7e
arpan <arpan.rec@gmail.com>
2024-05-05 00:43:35 +0530

    feat: added fd-find and neovim

--- .script.d/tools.sh
+++ .script.d/tools.sh
@@ -6,11 +6,13 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q -y
 # shellcheck source=/dev/null
 source "$HOME/.cargo/env"
 cargo install ripgrep
+cargo install fd-find
 
 curl -sSL https://install.python-poetry.org | python3 -
 
 npm i -g yarn
 npm i -g bw
+npm i -g neovim
 
 go install golang.org/x/tools/gopls@latest
 go install mvdan.cc/sh/v3/cmd/gosh@latest

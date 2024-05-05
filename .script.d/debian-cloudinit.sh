d14ffa43dab5a5bd142cc8197877b6375d5d2bd6
arpan <arpan.rec@gmail.com>
2024-05-06 02:09:51 +0530

    fix: Update ansible-galaxy collections and roles versions

--- .script.d/debian-cloudinit.sh
+++ .script.d/debian-cloudinit.sh
@@ -61,7 +61,7 @@ pip install ansible --upgrade
 # ansible-galaxy collection install arpanrec.nebula:5.2.4
 
 apt install -y git
-ansible-galaxy collection install git+https://github.com/arpanrec/arpanrec.nebula.git,5.2.4
+ansible-galaxy collection install git+https://github.com/arpanrec/arpanrec.nebula.git,5.3.1
 ansible-galaxy role install git+https://github.com/geerlingguy/ansible-role-docker.git,7.1.0,geerlingguy.docker
 
 tee "${ANSIBLE_INVENTORY}" <<EOF >/dev/null

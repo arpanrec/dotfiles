09a04847d7f17bc2fbc48bf6c961f936adeb2e0f
arpan <arpan.rec@gmail.com>
2024-05-10 20:47:39 +0530

    feat: pulumi

--- .script.d/tools.sh
+++ .script.d/tools.sh
@@ -1,6 +1,33 @@
 #!/usr/bin/env bash
 set -ex
 
+install_neovim() {
+
+    NEOVIM_GIT_CLONE_DIR="${NEOVIM_GIT_CLONE_DIR:-"/tmp/neovim-src-$(date +%s)"}"
+
+    if [ "$(id -u)" -eq 0 ]; then
+        export NEOVIM_INSTALL_DIR=${NEOVIM_INSTALL_DIR:-"/usr/local"}
+    fi
+
+    NEOVIM_INSTALL_DIR="${NEOVIM_INSTALL_DIR:-"${HOME}/.local"}"
+    NEOVIM_VERSION="${NEOVIM_VERSION:-"v0.9.5"}"
+
+    mkdir -p "$(dirname "${NEOVIM_GIT_CLONE_DIR}")"
+    git clone https://github.com/neovim/neovim.git --single-branch \
+        --branch="${NEOVIM_VERSION}" --depth 1 "${NEOVIM_GIT_CLONE_DIR}"
+
+    cd "${NEOVIM_GIT_CLONE_DIR}"
+
+    rm -rf "${NEOVIM_INSTALL_DIR}/state/nvim"
+    rm -rf "${NEOVIM_INSTALL_DIR}/share/nvim"
+    rm -rf "${NEOVIM_INSTALL_DIR}/bin/nvim"
+    rm -rf "${HOME}/.cache/nvim"
+
+    make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=${NEOVIM_INSTALL_DIR}"
+    make install
+
+}
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q -y
 
 # shellcheck source=/dev/null
@@ -19,4 +46,6 @@ go install mvdan.cc/sh/v3/cmd/gosh@latest
 go install github.com/mikefarah/yq/v4@latest
 go install github.com/minio/mc@latest
 
-"${HOME}/.script.d/neovim-from-source.sh"
+curl -fsSL https://get.pulumi.com | sh
+
+install_neovim

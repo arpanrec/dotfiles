1e72468e789c9b558424ec0b12b122d4ee194606
arpan <arpan.rec@gmail.com>
2024-05-15 01:27:32 +0530

    logging

--- .script.d/tools.sh
+++ .script.d/tools.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
-set -ex
+set -e
+
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Root user detected!!!! Error"
+    exit 1
+fi
 
 install_neovim() {
+    echo "Installing Neovim"
+
+    echo "Checking for CPU count"
     CPUCOUNT=$(grep -c "^processor" /proc/cpuinfo)
+    echo "CPU count is ${CPUCOUNT}"
 
     NEOVIM_GIT_CLONE_DIR="${NEOVIM_GIT_CLONE_DIR:-"/tmp/neovim-src-$(date +%s)"}"
 
@@ -12,25 +21,30 @@ install_neovim() {
 
     NEOVIM_INSTALL_DIR="${NEOVIM_INSTALL_DIR:-"${HOME}/.local"}"
     NEOVIM_VERSION="${NEOVIM_VERSION:-"v0.9.5"}"
+    echo "Neovim version is ${NEOVIM_VERSION}"
 
     mkdir -p "$(dirname "${NEOVIM_GIT_CLONE_DIR}")"
+
+    echo "Cloning Neovim to ${NEOVIM_GIT_CLONE_DIR}"
     git clone https://github.com/neovim/neovim.git --single-branch \
         --branch="${NEOVIM_VERSION}" --depth 1 "${NEOVIM_GIT_CLONE_DIR}"
 
-    cd "${NEOVIM_GIT_CLONE_DIR}"
+    cd "${NEOVIM_GIT_CLONE_DIR}" || exit 1
 
     rm -rf "${NEOVIM_INSTALL_DIR}/state/nvim"
     rm -rf "${NEOVIM_INSTALL_DIR}/share/nvim"
     rm -rf "${NEOVIM_INSTALL_DIR}/bin/nvim"
     rm -rf "${HOME}/.cache/nvim"
 
+    echo "Building Neovim"
     make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=${NEOVIM_INSTALL_DIR}" -j"${CPUCOUNT}"
     make install
 
 }
 
 which_python() {
-    declare -a PYTHON_VERSIONS=("python3.13" "python3.12" "python3.11" "python3.10" "python3.9" "python3.8" "python3.7" "python3.6")
+    declare -a PYTHON_VERSIONS=("python3.13" "python3.12" "python3.11" "python3.10"
+        "python3.9" "python3.8" "python3.7" "python3.6")
 
     for python_version in "${PYTHON_VERSIONS[@]}"; do
         if command -v "${python_version}" &>/dev/null; then
@@ -40,30 +54,50 @@ which_python() {
     done
 
     echo "Supported Python version not found, Only Python3.6+ >< 4 is supported"
-
+    exit 1
 }
+
+echo "Installing tools"
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q -y
 # shellcheck source=/dev/null
 source "${HOME}/.cargo/env"
-cargo install ripgrep
-cargo install fd-find
+
+declare -a cargo_packages=("ripgrep" "fd-find")
+
+if command -v cargo &>/dev/null; then
+    echo "Installing cargo packages"
+    for cargo_package in "${cargo_packages[@]}"; do
+        echo "Installing cargo package ${cargo_package}"
+        cargo install "${cargo_package}"
+    done
+fi
 
 curl -sSL https://install.python-poetry.org | $(which_python) -
 
+declare -a npm_packages=("yarn" "bw" "neovim")
 if command -v npm &>/dev/null; then
-
-    npm i -g yarn
-    npm i -g bw
-    npm i -g neovim
+    echo "Installing npm packages"
+    for npm_package in "${npm_packages[@]}"; do
+        echo "Installing npm package globally ${npm_package}"
+        npm install -g "${npm_package}"
+    done
 fi
 
+declare -a go_packages=(
+    "golang.org/x/tools/gopls@latest"
+    "mvdan.cc/sh/v3/cmd/gosh@latest"
+    "github.com/mikefarah/yq/v4@latest"
+    "github.com/minio/mc@latest"
+    "github.com/jesseduffield/lazygit@latest"
+)
+
 if command -v go &>/dev/null; then
-    go install golang.org/x/tools/gopls@latest
-    go install mvdan.cc/sh/v3/cmd/gosh@latest
-    go install github.com/mikefarah/yq/v4@latest
-    go install github.com/minio/mc@latest
-    go install github.com/jesseduffield/lazygit@latest
+    echo "Installing go packages"
+    for go_package in "${go_packages[@]}"; do
+        echo "Installing go package ${go_package}"
+        go install "${go_package}"
+    done
 fi
 
 install_neovim

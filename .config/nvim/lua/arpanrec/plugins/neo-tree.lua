db3309c884fd7763948595355069e5be00662657
arpan <arpan.rec@gmail.com>
2024-06-01 18:29:44 +0530

    ip

--- .config/nvim/lua/arpanrec/plugins/neo-tree.lua
+++ .config/nvim/lua/arpanrec/plugins/neo-tree.lua
@@ -52,8 +52,8 @@ return {
             enable_diagnostics = true,
             -- enable_normal_mode_for_inputs = false,                             -- Enable normal mode for input dialogs.
             open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
-            sort_case_insensitive = false,                               -- used when sorting files and directories in the tree
-            sort_function = nil,                                         -- use a custom function for sorting files and directories in the tree
+            sort_case_insensitive = false,                                     -- used when sorting files and directories in the tree
+            sort_function = nil,                                               -- use a custom function for sorting files and directories in the tree
             -- sort_function = function (a,b)
             --       if a.type == b.type then
             --           return a.path > b.path
@@ -224,11 +224,11 @@ return {
                     },
                 },
                 follow_current_file = {
-                    enabled = false, -- This will find and focus the file in the active buffer every time
+                    enabled = false,         -- This will find and focus the file in the active buffer every time
                     --               -- the current file is changed while the tree is open.
                     leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
                 },
-                group_empty_dirs = false, -- when true, empty folders will be grouped together
+                group_empty_dirs = false,    -- when true, empty folders will be grouped together
                 -- netrw disabled, opening a directory opens neo-tree
                 -- in whatever position is specified in window.position
                 -- "open_current",  -- netrw disabled, opening a directory opens within the
@@ -272,11 +272,11 @@ return {
             },
             buffers = {
                 follow_current_file = {
-                    enabled = true, -- This will find and focus the file in the active buffer every time
+                    enabled = true,          -- This will find and focus the file in the active buffer every time
                     --              -- the current file is changed while the tree is open.
                     leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
                 },
-                group_empty_dirs = true, -- when true, empty folders will be grouped together
+                group_empty_dirs = true,     -- when true, empty folders will be grouped together
                 show_unloaded = true,
                 window = {
                     mappings = {

176a05f6f8fba3d13176211f187ee60f2845dbb4
arpan <arpan.rec@gmail.com>
2024-10-23 22:35:07 +0530

    Update ansible collections

--- .script.d/debian-cloudinit.sh
+++ .script.d/debian-cloudinit.sh
@@ -63,13 +63,13 @@ pip3 install --upgrade pip
 pip3 install setuptools-rust wheel setuptools --upgrade
 pip3 install ansible hvac --upgrade
 
-ansible-galaxy collection install git+https://github.com/arpanrec/arpanrec.nebula.git,1.1.0
+ansible-galaxy collection install git+https://github.com/arpanrec/arpanrec.nebula.git,1.2.0
 
 ansible-galaxy collection install git+https://github.com/ansible-collections/community.general.git,9.4.0
-ansible-galaxy collection install git+https://github.com/ansible-collections/community.crypto.git,2.22.0
+ansible-galaxy collection install git+https://github.com/ansible-collections/community.crypto.git,2.22.2
 ansible-galaxy collection install git+https://github.com/ansible-collections/amazon.aws.git,8.2.1
-ansible-galaxy collection install git+https://github.com/ansible-collections/community.docker.git,3.12.1
-ansible-galaxy collection install git+https://github.com/ansible-collections/ansible.posix.git,1.6.0
+ansible-galaxy collection install git+https://github.com/ansible-collections/community.docker.git,4.0.0
+ansible-galaxy collection install git+https://github.com/ansible-collections/ansible.posix.git,1.6.2
 ansible-galaxy collection install git+https://github.com/kewlfft/ansible-aur.git,v0.11.1
 
 ansible-galaxy role install git+https://github.com/geerlingguy/ansible-role-docker.git,7.4.1,geerlingguy.docker

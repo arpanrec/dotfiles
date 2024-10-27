27867c1f0cbfc715b1b8a163e862d9215c7173b1
arpan <arpan.rec@gmail.com>
2024-10-27 20:24:43 +0530

    Update ansible collections to version 1.8.0

--- .script.d/server-workspace.sh
+++ .script.d/server-workspace.sh
@@ -152,16 +152,11 @@ pip3 install --upgrade pip
 pip3 install setuptools-rust wheel setuptools --upgrade
 pip3 install ansible hvac --upgrade
 
-ansible-galaxy collection install git+https://github.com/arpanrec/arpanrec.nebula.git,1.6.0
-
-ansible-galaxy collection install git+https://github.com/ansible-collections/community.general.git,9.4.0
-ansible-galaxy collection install git+https://github.com/ansible-collections/community.crypto.git,2.22.2
-ansible-galaxy collection install git+https://github.com/ansible-collections/amazon.aws.git,8.2.1
-ansible-galaxy collection install git+https://github.com/ansible-collections/community.docker.git,4.0.0
-ansible-galaxy collection install git+https://github.com/ansible-collections/ansible.posix.git,1.6.2
-ansible-galaxy collection install git+https://github.com/kewlfft/ansible-aur.git,v0.11.1
-
-ansible-galaxy role install git+https://github.com/geerlingguy/ansible-role-docker.git,7.4.1,geerlingguy.docker
+export NEBULA_VERSION=1.8.0
+curl "https://raw.githubusercontent.com/arpanrec/arpanrec.nebula/refs/tags/${NEBULA_VERSION}/requirements.yml" \
+    -o "/tmp/requirements-${NEBULA_VERSION}.yml"
+ansible-galaxy install -r "/tmp/requirements-${NEBULA_VERSION}.yml"
+ansible-galaxy collection install "git+https://github.com/arpanrec/arpanrec.nebula.git,${NEBULA_VERSION}"
 
 echo "MMC_SERVER_WORKSPACE_JSON :: ${MMC_SERVER_WORKSPACE_JSON}"
 echo "Check if ${MMC_SERVER_WORKSPACE_JSON} exists"

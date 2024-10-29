014a62ed5d13025edd9d57a20e3db3a155adb5fc
arpan <arpan.rec@gmail.com>
2024-10-29 16:09:17 +0530

    Update dot-install.sh to support cloning dotfiles repository using SSH if SSH key is present

--- .script.d/dot-install.sh
+++ .script.d/dot-install.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+git_remote_url="https://github.com/arpanrec/dotfiles.git"
+
+if [ -f "${HOME}/.ssh/github.com" ]; then
+    git_remote_url="git@github.com:arpanrec/dotfiles.git"
+fi
+
 rm -rf "${HOME}/.dotfiles"
-git clone --bare https://github.com/arpanrec/dotfiles.git "${HOME}/.dotfiles"
+git clone --bare "${git_remote_url}" "${HOME}/.dotfiles"
 git --git-dir="${HOME}/.dotfiles" --work-tree="${HOME}" config --local status.showUntrackedFiles no
 git --git-dir="${HOME}/.dotfiles" --work-tree="${HOME}" checkout main --force
 git --git-dir="${HOME}/.dotfiles" --work-tree="${HOME}" config \

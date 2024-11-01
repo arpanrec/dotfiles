aa470346816a5880819e34fa02ad6c6a2d361439
arpan <arpan.rec@gmail.com>
2024-11-02 01:53:52 +0530

    Refactor dot-install script to improve clarity and structure

--- .script.d/dot-install.sh
+++ .script.d/dot-install.sh
@@ -1,15 +1,42 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git_remote_url="https://github.com/arpanrec/dotfiles.git"
+dotfiles_git_remote="https://github.com/arpanrec/dotfiles.git"
+dotfiles_dir="${HOME}/.dotfiles"
+
+printf "\n\n================================================================================\n"
+echo "Installing dotfiles from ${dotfiles_git_remote}"
+echo "--------------------------------------------------------------------------------"
 
 if [ -f "${HOME}/.ssh/github.com" ]; then
-    git_remote_url="git@github.com:arpanrec/dotfiles.git"
+    dotfiles_git_remote="git@github.com:arpanrec/dotfiles.git"
+    printf "\n\n================================================================================\n"
+    echo "Using SSH key for GitHub at ${HOME}/.ssh/github.com and changing remote URL to ${dotfiles_git_remote}"
+    echo "--------------------------------------------------------------------------------"
 fi
 
+printf "\n\n================================================================================\n"
+echo "Removing existing dotfiles directory at ${dotfiles_dir}"
+echo "--------------------------------------------------------------------------------"
 rm -rf "${HOME}/.dotfiles"
-git clone --bare "${git_remote_url}" "${HOME}/.dotfiles"
+
+printf "\n\n================================================================================\n"
+echo "Cloning dotfiles from ${dotfiles_git_remote} to ${dotfiles_dir} as a bare repository"
+echo "--------------------------------------------------------------------------------"
+git clone --bare "${dotfiles_git_remote}" "${HOME}/.dotfiles"
+
+printf "\n\n================================================================================\n"
+echo "Setting status.showUntrackedFiles to no"
+echo "--------------------------------------------------------------------------------"
 git --git-dir="${HOME}/.dotfiles" --work-tree="${HOME}" config --local status.showUntrackedFiles no
+
+printf "\n\n================================================================================\n"
+echo "Checking out main branch with force"
+echo "--------------------------------------------------------------------------------"
 git --git-dir="${HOME}/.dotfiles" --work-tree="${HOME}" checkout main --force
+
+printf "\n\n================================================================================\n"
+echo "Adding remote origin with fetch refspec"
+echo "--------------------------------------------------------------------------------"
 git --git-dir="${HOME}/.dotfiles" --work-tree="${HOME}" config \
     --local remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"

5dac11336f853e806ba5871808f1bdf226591ecd
arpan <arpan.rec@gmail.com>
2024-11-06 02:56:08 +0530

    Debug

--- .script.d/linode-stack-script.sh
+++ .script.d/linode-stack-script.sh
@@ -49,15 +49,17 @@ sudo apt update && sudo apt install -y python3-venv python3-pip git curl ca-cert
 sudo systemctl enable --now cron
 
 if [ -z "${CLOUD_INIT_WEB_SERVER_FQDN:-}" ]; then
-    echo "CLOUD_INIT_WEB_SERVER_FQDN is not set"
+    log_message "CLOUD_INIT_WEB_SERVER_FQDN is not set"
 else
-    echo "CLOUD_INIT_WEB_SERVER_FQDN is set to ${CLOUD_INIT_WEB_SERVER_FQDN}"
+    log_message "CLOUD_INIT_WEB_SERVER_FQDN is set to ${CLOUD_INIT_WEB_SERVER_FQDN}"
 
     IPADDR=$(/sbin/ifconfig eth0 | awk '/inet / { print $2 }' | sed 's/addr://')
     sed -i "/${IPADDR}.*/d" /etc/hosts
     echo "${IPADDR} ${CLOUD_INIT_WEB_SERVER_FQDN}" | sudo tee -a /etc/hosts
 fi
 
+exit 1
+
 sudo -E -H -u root bash -c '/bin/bash <(curl -sSL \
-    https://raw.githubusercontent.com/arpanrec/dotfiles/refs/heads/main/.script.d/linode-stack-script.sh)' |
+    https://raw.githubusercontent.com/arpanrec/dotfiles/refs/heads/main/.script.d/debian-cloudinit.sh)' |
     tee -a /root/linode-stack-script.log

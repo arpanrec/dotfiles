3d86d6f5f0926c6f26c980f3fd8f8db5fe502757
arpan <arpan.rec@gmail.com>
2024-11-12 01:15:38 +0530

    Change environment variable export format in linode-stack-script.sh

--- .script.d/linode-stack-script.sh
+++ .script.d/linode-stack-script.sh
@@ -95,7 +95,7 @@ declare -A env_vars=(
 
 for var in "${!env_vars[@]}"; do
     sed -i "/^${var}=.*/d" /etc/environment
-    echo "${var}=${env_vars[$var]}" | tee -a /etc/environment
+    echo "export ${var}=${env_vars[$var]}" | tee -a /etc/environment
 done
 
 log_message "Sourcing /etc/environment after setting environment variables"

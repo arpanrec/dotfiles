0f82d79fd9b894a32ef64f9de5ab5e4ad27bbf88
arpan <arpan.rec@gmail.com>
2024-12-30 20:41:14 +0530

    Set locale environment variables in debian-cloudinit script

--- .script.d/debian-cloudinit.sh
+++ .script.d/debian-cloudinit.sh
@@ -9,6 +9,21 @@ debian-cloudinit: \
 %s\n--------------------------------------------------------------------------------\n\n" "$(date)" "$*"
 }
 
+export LC_ALL="en_US.UTF8"
+export LC_TIME="en_US.UTF8"
+export LC_MONETARY="en_US.UTF8"
+export LC_CTYPE="en_US.UTF8"
+export LC_COLLATE="en_US.UTF8"
+export LC_ADDRESS="en_US.UTF8"
+export LC_TELEPHONE="en_US.UTF8"
+export LC_MESSAGES="en_US.UTF8"
+export LC_NAME="en_US.UTF8"
+export LC_MEASUREMENT="en_US.UTF8"
+export LC_IDENTIFICATION="en_US.UTF8"
+export LC_NUMERIC="en_US.UTF8"
+export LC_PAPER="en_US.UTF8"
+export LANG="en_GB.UTF-8"
+
 export -f log_message
 
 log_message "Starting"

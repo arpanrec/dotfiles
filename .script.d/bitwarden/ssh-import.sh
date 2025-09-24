d40ca5136f57d34b8a2fe07ff50b743c53797b9c
arpan <me@arpanrec.com>
2025-09-24 19:42:14 +0530

    Move gitea server

--- .script.d/bitwarden/ssh-import.sh
+++ .script.d/bitwarden/ssh-import.sh
@@ -84,7 +84,7 @@ declare -a bw_items=(
     "GitLab - arpanrec" gitlab.com OPENSSH_KEY_PASSPHRASE
     "Linode - arpanrecme" linode_ssh_key OPENSSH_KEY_PASSPHRASE
     "Router - BLR Flat" r1-tpla9v6.key OPENSSH_KEY_PASSPHRASE
-    "Gitea - blr-home - rb4-m2" id_gitea_blr_home_rb4_m2 OPENSSH_KEY_PASSPHRASE
+    "Gitea - blr-home - rb5-m3" id_gitea_blr_home_rb5_m3 OPENSSH_KEY_PASSPHRASE
 )
 
 for ((i = 0; i < ${#bw_items[@]}; i += 3)); do

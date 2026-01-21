7fbe4cc5a87001865ee662055207ffc4f38b9c52
arpan <me@arpanrec.com>
2026-01-22 02:11:56 +0530

    scripts to local bin

--- .script.d/myfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-__media_dir="./"
-
-echo "Setting up media ownership and permissions"
-sudo chown -R --verbose "$(id -u)":"$(id -g)" "${__media_dir}"
-sudo find "${__media_dir}" -type d -print -exec chmod 750 {} \;
-sudo find "${__media_dir}" -type f -print -exec chmod 640 {} \;

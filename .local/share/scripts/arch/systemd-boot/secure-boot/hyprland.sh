2a434cdd5f1e99ffcdeb43f8e4adca82a3f279fb
arpan <me@arpanrec.com>
2026-01-26 00:32:50 +0530

    Add `wildmidi` and `ttf-fantasque-sans-mono` to Arch Hyprland package list

--- .local/share/scripts/arch/systemd-boot/secure-boot/hyprland.sh
+++ .local/share/scripts/arch/systemd-boot/secure-boot/hyprland.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 PACMAN_PACKAGES+=('hyprland' 'kwalletmanager' 'kwallet-pam' 'sddm' 'polkit' 'hyprpolkitagent' 'xorg-xwayland'
     'xorg-xeyes' 'xorg-xlsclients' 'xdg-desktop-portal-hyprland' 'qt5-wayland' 'qt6-wayland' 'hyprpaper'
     'hyprlauncher' 'dolphin' 'dunst' 'kitty' 'kate' 'konsole' 'qt6-declarative' 'qtkeychain-qt6'
-    'wl-clipboard' 'cliphist' 'hyprshot' 'mpd' 'vice' 'ncmpcpp')
+    'wl-clipboard' 'cliphist' 'hyprshot' 'mpd' 'vice' 'ncmpcpp' 'wildmidi')
 PACMAN_PACKAGES+=('networkmanager-openvpn' 'libnma' 'network-manager-applet' 'networkmanager-openconnect')
 PACMAN_PACKAGES+=('noto-fonts' 'noto-fonts-cjk' 'noto-fonts-emoji' 'noto-fonts-extra' 'waybar' 'otf-font-awesome'
-    'adobe-source-sans-fonts' 'ttf-jetbrains-mono-nerd')
+    'adobe-source-sans-fonts' 'ttf-jetbrains-mono-nerd' 'ttf-fantasque-sans-mono')
 
 PACMAN_PACKAGES+=('gtkmm3' 'jsoncpp' 'libsigc++' 'fmt' 'chrono-date' 'spdlog' 'gtk3' 'gobject-introspection'
     'libgirepository' 'libpulse' 'libnl' 'libappindicator-gtk3' 'libdbusmenu-gtk3' 'libmpdclient' 'sndio' 'libevdev'

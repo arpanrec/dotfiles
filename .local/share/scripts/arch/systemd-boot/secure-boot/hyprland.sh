34f5bbafddaf6176a08e46efcc1a2abd30a00e8a
arpan <me@arpanrec.com>
2026-01-31 02:04:35 +0530

    update Hyprland script: revise AUR package list and adjust yay flags for cleaner installation

--- .local/share/scripts/arch/systemd-boot/secure-boot/hyprland.sh
+++ .local/share/scripts/arch/systemd-boot/secure-boot/hyprland.sh
@@ -75,11 +75,11 @@ pacman -S --needed --noconfirm "${PACMAN_PACKAGES[@]}"
 systemctl enable sddm cups avahi-daemon.service
 systemctl set-default graphical.target
 
-AUR_PACKAGES=('google-chrome' 'brave-bin' 'sublime-text-4' 'onlyoffice-bin' 'yubico-authenticator-bin'
-    'sddm-silent-theme-git')
+# 'google-chrome'
+AUR_PACKAGES=('brave-bin' 'onlyoffice-bin' 'yubico-authenticator-bin' 'sddm-silent-theme-git')
 
 sudo -H -u arch-yay-installer-user bash -c "cd ~ && \
-        yay -S --save --nodiffmenu --nocleanmenu --noanswerlist --answerupgrade All --noconfirm --needed $(printf " %s" "${AUR_PACKAGES[@]}")"
+        yay -S --answerclean None --answerdiff None --noconfirm --needed $(printf " %s" "${AUR_PACKAGES[@]}")"
 
 sed -i 's|^ConfigFile=configs/default\.conf$|ConfigFile=configs/rei.conf|' \
     /usr/share/sddm/themes/silent/metadata.desktop

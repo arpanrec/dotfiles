4fdb5aec5a2ba39f1d8132d21547bb6c93993c67
arpan <me@arpanrec.com>
2026-02-02 16:24:30 +0530

    fix: update GPG pinentry program configuration in Hyprland script

--- .config/hypr/set-gpg-pinentry-program.sh
+++ .config/hypr/set-gpg-pinentry-program.sh
@@ -5,6 +5,5 @@ mkdir -p "${HOME}/.gnupg"
 chmod 700 "${HOME}/.gnupg"
 touch "${HOME}/.gnupg/gpg-agent.conf"
 chmod 600 "${HOME}/.gnupg/gpg-agent.conf"
-if [[ -f /usr/bin/pinentry-qt ]]; then
-    sed -i 's/^pinentry-program.*/pinentry-program \/usr\/bin\/pinentry-qt/' "${HOME}/.gnupg/gpg-agent.conf"
-fi
+sed -i 's/^pinentry-program.*/d' "${HOME}/.gnupg/gpg-agent.conf"
+echo 'pinentry-program /usr/bin/pinentry-qt' | tee -a "${HOME}/.gnupg/gpg-agent.conf"

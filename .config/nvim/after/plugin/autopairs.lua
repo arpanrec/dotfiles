22f41aaddfb19daef02d810699adf0110a2d3e00
arpan <me@arpanrec.com>
2026-02-06 19:12:41 +0530

    remove outdated packages

--- .config/nvim/after/plugin/autopairs.lua
+++ .config/nvim/after/plugin/autopairs.lua
@@ -1,6 +1,5 @@
 local cmp = require("cmp")
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local ts_utils = require("nvim-treesitter.ts_utils")
 
 local ts_node_func_parens_disabled = {
     -- ecma
@@ -11,14 +10,6 @@ local ts_node_func_parens_disabled = {
 
 local default_handler = cmp_autopairs.filetypes["*"]["("].handler
 cmp_autopairs.filetypes["*"]["("].handler = function(char, item, bufnr, rules, commit_character)
-    local node_type = ts_utils.get_node_at_cursor():type()
-    if ts_node_func_parens_disabled[node_type] then
-        if item.data then
-            item.data.funcParensDisabled = true
-        else
-            char = ""
-        end
-    end
     default_handler(char, item, bufnr, rules, commit_character)
 end
 

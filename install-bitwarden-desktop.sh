989b8224ddb3e24b65bf7643ba4a0e193010fd4c
arpan <me@arpanrec.com>
2026-02-07 22:22:13 +0530

    Update scripts: enable Wayland support for VS Code and Bitwarden, bump Bitwarden version

--- install-bitwarden-desktop.sh
+++ install-bitwarden-desktop.sh
@@ -14,7 +14,7 @@ x86_64)
 esac
 
 TMP_DOWNLOAD_DIRECTORY="${HOME}/.tmp/from_dotfiles_bin"
-LATEST_VERSION="2025.12.0"
+LATEST_VERSION="2026.1.0"
 APPIMAGE_FILE_NAME="Bitwarden-${LATEST_VERSION}-${DOWNLOAD_ARCH_KEY}.AppImage"
 APPIMAGE_INSTALL_DIRECTORY="${HOME}/.local/share/bitwarden-desktop"
 DOWNLOAD_URI="https://github.com/bitwarden/clients/releases/download/desktop-v${LATEST_VERSION}/${APPIMAGE_FILE_NAME}"
@@ -41,7 +41,7 @@ tee "${HOME}/.local/share/applications/bitwarden.desktop" <<EOF
 Name=Bitwarden
 GenericName=Password Manager
 Comment=A secure and free password manager for all of your devices.
-Exec=${APPIMAGE_INSTALL_DIRECTORY}/bitwarden-desktop.AppImage %u
+Exec=${APPIMAGE_INSTALL_DIRECTORY}/bitwarden-desktop.AppImage --enable-features=UseOzonePlatform --ozone-platform=wayland %u
 Terminal=false
 MimeType=x-scheme-handler/bitwarden
 Type=Application

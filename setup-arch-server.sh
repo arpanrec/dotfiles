e9b4d256fe70e000833914795f5ab1a2b6582a7a
arpan <me@arpanrec.com>
2026-03-21 01:40:05 +0530

    Update `setup-arch-server.sh`: refine systemd-boot secure boot prompt wording

--- setup-arch-server.sh
+++ setup-arch-server.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 echo "Starting setup"
 echo "Allowed hosts are: s1-dev, s2-dev"
 
-read -p "Reinstall systemd-boot with secure boot enabled: (y/Y)" -r IS_SYSTEMD_SECURE_BOOT
+read -p "Boot loader manged my this script, [systemd secure boot]: (y/Y)" -r IS_SYSTEMD_SECURE_BOOT
 
 IS_NVIDIA_DRM=n
 if lspci | grep -E "(VGA|3D)" | grep -E "(NVIDIA|GeForce)"; then

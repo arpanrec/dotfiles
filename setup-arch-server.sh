fd43a0ba3d9941a942f62f821b87fd6f39e0f676
arpan <me@arpanrec.com>
2026-03-23 02:25:03 +0530

    remove plymoth

--- setup-arch-server.sh
+++ setup-arch-server.sh
@@ -411,8 +411,8 @@ default_options=""
 fallback_image="/boot/initramfs-linux-fallback.img"
 fallback_options="-S autodetect"
 EOF
-
-    sed -i 's/^HOOKS=.*/HOOKS=(base systemd plymouth autodetect microcode modconf kms keyboard keymap sd-vconsole block sd-encrypt lvm2 filesystems fsck)/' \
+    # plymouth
+    sed -i 's/^HOOKS=.*/HOOKS=(base systemd autodetect microcode modconf kms keyboard keymap sd-vconsole block sd-encrypt lvm2 filesystems fsck)/' \
         /etc/mkinitcpio.conf
 
     mkinitcpio -P

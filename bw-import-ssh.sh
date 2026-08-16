bd4a8243dc4f9466e818293c44faa16d11edd82d
arpan <me@arpanrec.com>
2026-08-16 17:32:10 +0530

    Rename Bitwarden item for DevOPS Server in bw-import-ssh.sh

--- bw-import-ssh.sh
+++ bw-import-ssh.sh
@@ -110,7 +110,7 @@ declare -a bw_items=(
     "GitLab - arpanrec" gitlab.com OPENSSH_KEY_PASSPHRASE
     "Linode - arpanrecme" linode_ssh_key OPENSSH_KEY_PASSPHRASE
     "Router - BLR Flat - r1-tpla9v6" r1-tpla9v6.key OPENSSH_KEY_PASSPHRASE
-    "SCM - blr-home" id_scm_blr_home OPENSSH_KEY_PASSPHRASE
+    "DevOPS Server - blr-home - gitea" id_scm_blr_home OPENSSH_KEY_PASSPHRASE
 )
 
 for ((i = 0; i < ${#bw_items[@]}; i += 3)); do

f1c582140aa2ca976dd39f356aacbe3d2f00c3eb
arpan <me@arpanrec.com>
2026-09-07 22:02:44 +0530

    Remove "uv" from cargo binstall command in install script

--- install-rustup.sh
+++ install-rustup.sh
@@ -23,7 +23,7 @@ curl -L --proto '=https' --tlsv1.2 -sSf --connect-timeout 10 --max-time 300 \
 
 if command -v cargo &>/dev/null; then
     echo "Installing cargo packages"
-    echo y | cargo binstall "fd-find" "ripgrep" "uv" --force
+    echo y | cargo binstall "fd-find" "ripgrep" --force
 fi
 
 echo "Completed"
